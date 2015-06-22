50646ea20faa4b73f493ba08c9201153592afb66
Yegor Pomortsev <yegor@pomortsev.com>
2015-06-22 14:19:59 -0700

    Configure S3 bucket region properly

--- config/app/config.app.aws.js
+++ config/app/config.app.aws.js
@@ -3,10 +3,10 @@
 module.exports = function(config, options) {
   if (options.appEnv === 'prod') {
     config.aws.options.params.Bucket = 'mycoolapp.apigee.com';
-    config.aws.options.params.region = 'us-east-1';
+    config.aws.options.region = 'us-east-1';
   } else if (options.appEnv === 'e2e') {
     config.aws.options.params.Bucket = 'mycoolapp.e2e.apigee.net';
-    config.aws.options.params.region = 'us-west-2';
+    config.aws.options.region = 'us-west-2';
   }
 
   return config;
